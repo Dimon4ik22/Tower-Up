5b4470f1caaf0ecd2c3b5ac8c56470f49bb6584c
Lorenhow <CrashBuilder@yandex.ru>
2022-05-19 13:10:17 +0300

    Update 1.2
    Наконец-то удалось разобраться с возвращалкой карты - теперь они плавно плывут к спавнеру(если вам не нравится, где карта - подвигайте спавнер и будет вам счастье). Сделаны правки в коллайдерах и методах "хватания" карт. Сделал более адекватный таймер в спавнере.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -18,12 +18,14 @@ public class Card : MonoBehaviour
     private Vector3 startpoint;
     private Vector3 offset;
     private Vector3 cardpoint;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        startpoint = new Vector3(0, 0, 0);
+        startpoint = GameObject.FindWithTag("Respawn").transform.position; ;
         mainpanel = GameObject.FindWithTag("Game_Panel");
         mainscript = mainpanel.GetComponent<Game>();
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     //счётчики коллизии.
@@ -59,7 +61,7 @@ public class Card : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-            if (targetObject)
+            if (targetObject && targetObject.CompareTag("Card"))
             {
                 SelectedCard = targetObject.transform.gameObject;
                 offset = SelectedCard.transform.position - mousePosition;
@@ -115,21 +117,19 @@ public class Card : MonoBehaviour
             }
             else
             {
-                StartCoroutine(CardReturn());
+                //задание карте движения к спавнеру.
+                rb.velocity = (startpoint - SelectedCard.transform.position);
+                //сброс размеров и вращения карты, отмена выбора.
+                SelectedCard.transform.rotation = Quaternion.Euler(0, 0, 0);
+                SelectedCard.transform.localScale = new Vector3((float)0.5, (float)0.5, (float)1);
+                SelectedCard = null;
             }
-        }
-    }
+        }  
 
-    //возвращение карты в нули.
-    IEnumerator CardReturn()
-    {
-        while (SelectedCard.transform.position != startpoint)
-        {
-                SelectedCard.transform.position = Vector3.Lerp(SelectedCard.transform.position, startpoint, (float)0.5);
-        }
-        SelectedCard.transform.rotation = Quaternion.Euler(0, 0, 0);
-        SelectedCard.transform.localScale = new Vector3((float)0.5, (float)0.5, (float)1);
-        SelectedCard = null;
-        yield return new WaitForEndOfFrame();
+         //действия при возвращении карты в нули.
+         if (Vector3.Distance(rb.position, startpoint) <= 0.1f)
+         {
+                rb.velocity = Vector3.zero;
+         }
     }
 }
